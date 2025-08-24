52c22b0daa54440042ab0089f078c9dea1d284a1
Manoel Tesar <manoel.tesar@gmail.com>
2025-08-24 02:54:11 +0200

    Workaround to make the config compile without error

--- platforms/chibios/drivers/vendor/RP/RP2040/ps2_vendor.c
+++ platforms/chibios/drivers/vendor/RP/RP2040/ps2_vendor.c
@@ -1,6 +1,7 @@
 // Copyright 2022 Marek Kraus (@gamelaster)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <stdio.h>
 #include "gpio.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
@@ -21,9 +22,14 @@
 #    endif
 #endif
 
-#if PS2_DATA_PIN + 1 != PS2_CLOCK_PIN
-#    error PS/2 clock pin must be data pin + 1!
-#endif
+//#if PS2_DATA_PIN + 1 != PS2_CLOCK_PIN
+//#    error PS/2 clock pin must be data pin + 1!
+//#endif
+
+#undef PS2_DATA_PIN
+#undef PS2_CLOCK_PIN
+#define PS2_DATA_PIN 8
+#define PS2_CLOCK_PIN 9
 
 static inline void pio_serve_interrupt(void);
 
